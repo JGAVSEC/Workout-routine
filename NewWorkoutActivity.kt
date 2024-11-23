ba6e997ea5f794ab4e532efe5cf5dd2c32cd3a0f
Jožef Gabrijel Avsec <129589249+JGAVSEC@users.noreply.github.com>
2024-11-23 13:57:51 +0100

    Back button
    Dodal back button za new workout, da te vrze na main screen

--- NewWorkoutActivity.kt
+++ NewWorkoutActivity.kt
@@ -1,11 +1,20 @@
 package com.example.myworkoutapp
-
+import android.widget.Button
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import android.widget.Toast
 
 class NewWorkoutActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_workout)
+        
+        // Find the back button by its ID
+        val backButton: Button = findViewById(R.id.backButton)
+
+        // Set an OnClickListener for the back button
+        backButton.setOnClickListener {
+            finish() // Finish the current activity and go back to MainActivity
+        }
     }
 }
